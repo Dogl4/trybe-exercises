5b256e571420bbd0a8b203c526f82a1ec43254f5
Dogl4 <doougllas@hotmail.com.br>
2021-06-23 18:22:17 -0300

    Adiciona do 1 ao 4, os programas

---
+++ 1_Fundamentos/bloco_4/exercises/1.js
@@ -0,0 +1,40 @@
+let a;
+let b;
+
+a = 5;
+b = 10;
+
+let adicao = a + b;
+console.log(`Adição. ${adicao}`);
+
+let subtracao = a - b;
+console.log(`Subtração. ${subtracao}`);
+
+let multiplicacao = a * b;
+console.log(`Multiplicação. ${multiplicacao}`);
+
+let divisao = a / b;
+console.log(`Divisão. ${divisao}`);
+
+let modulo = a % b;
+console.log(`Modulo. ${modulo}`);
+
+
+let c = 1;
+let d = 2;
+let e = 3;
+
+if (c > d) {
+  console.log(`O 1° número é maior. ${c}`);
+}else {
+  console.log(`O 2° número é maior. ${d}`);
+} 
+
+if (c > d && C > e) {
+  console.log(`O 1° número é maior. ${c}`);
+}else if (d > c && d > e){
+  console.log(`O 2° número é maior. ${d}`);
+} else{
+  console.log(`O 3° número é maior. ${e}`);
+}
+
