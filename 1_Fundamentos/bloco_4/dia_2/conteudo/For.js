8a1418cb83b315af015c60cedf54ccd7dc4de352
Dogl4 <doougllas@hotmail.com.br>
2021-06-24 15:40:30 -0300

    Adiciona exemplo For.js

---
+++ 1_Fundamentos/bloco_4/dia_2/conteudo/For.js
@@ -0,0 +1,13 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+console.log(cars[0]); // Saab
+console.log(cars[1]); // Volvo
+console.log(cars[2]); // BMW
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
+
+let grocery = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let i = 0; i < grocery.length; i += 1) {
+  console.log(grocery[i]);
+}
