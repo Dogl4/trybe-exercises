6676cb29c273959f269ae48552abff3b024ee203
Dogl4 <doougllas@hotmail.com.br>
2021-06-24 15:23:15 -0300

    Termina os exemplos de Arrays

---
+++ 1_Fundamentos/bloco_4/dia_2/conteudo/ArraysEx1.js
@@ -0,0 +1,12 @@
+// 1
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu;
+console.log(menuServices);
+
+// 2 Procure o índice do valor "Portfólio" do array menu:
+let indexOfPortfolio = menu.indexOf('Portfólio');
+console.log(indexOfPortfolio);
+
+//3 Adicione o valor "Contato" no final do array menu;
+menu.push('Contato');
+console.log(menu);
