1706d30f4964a6d5a0a08323127bddbdd366c11b
Dogl4 <doougllas@hotmail.com.br>
2021-07-22 23:42:36 -0300

    exercicio de retornar ternario, transformado em ternario.

--- 1_Fundamentos/bloco_7/exercises/parteII.js
+++ 1_Fundamentos/bloco_7/exercises/parteII.js
@@ -1,11 +1,7 @@
 const fatorial = (number) => {
   let seuFatorial;
   for (let i = number; i > 1; i -= 1) {
-   if (number === i) {
-     seuFatorial = i * (i - 1);
-   } else {
-     seuFatorial = seuFatorial * (i - 1);
-   }
+   number === i ? seuFatorial = i * (i - 1) : seuFatorial = seuFatorial * (i - 1);
   }
   return seuFatorial;
 };
