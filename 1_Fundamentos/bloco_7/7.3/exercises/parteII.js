717e68ff114c4b2d9817a9f33039a5f8d4fcd792
Dogl4 <doougllas@hotmail.com.br>
2021-07-26 18:43:35 -0300

    7.3 ES6 - exercicio 3, verificao, teste unitario.

--- 1_Fundamentos/bloco_7/7.3/exercises/parteII.js
+++ 1_Fundamentos/bloco_7/7.3/exercises/parteII.js
@@ -17,3 +17,5 @@ assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]); // verifica === ar
 
 // 2 Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
 assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]); // verifica, !== da entrada
+
+// 3 Verifique se o array passado por parâmetro não sofreu alterações
