c8d4d2472ea4475cd5a2f4bb447c8fa27c6c3b62
Dogl4 <doougllas@hotmail.com.br>
2021-07-29 01:28:26 -0300

    Adiciona exemplos, do dia 8.1, ES6-Higher-Order-Function.

---
+++ 1_Fundamentos/bloco_8/conteudo/Higher-Order-Function.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('#signup-button');
+
+const registerUser = () => {
+  console.log('Registrado com sucesso!');
+};
+
+button.addEventListener('click', registerUser);
+
+
+
+const button = document.querySelector('#signup-button');
+
+const registerUser = () => {
+  console.log('Registrado com sucesso!');
+};
+
+button.addEventListener('click', registerUser);
