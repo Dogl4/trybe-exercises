7e6701237e5255e930ba18c3c91b1cd2abbdebaa
Dogl4 <doougllas@hotmail.com.br>
2021-07-29 16:59:40 -0300

    8.2 conteudo parteII - Estrutura HOF, (elemento, index, array).

---
+++ 1_Fundamentos/bloco_8/8.2_HOF_forEach,find,some,every,sort/conteudo/parteII_Estrutura_HOF_array.js
@@ -0,0 +1,28 @@
+// const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate'}];
+// arrayOfValues.forEach((element) => {
+//   console.log('Cada elemento do array:', element);
+// });
+
+// arrayOfValues => Array percorrido
+// .forEach =>  HOF, pode ser .find, .some, .every
+// element => Element, no caso ele percorre todos.
+
+// const meuArray = [2,3,5,6,8,10];
+
+// meuArray.forEach((elemento) => {
+//   if (elemento % 2 === 0) {
+//     console.log(`${elemento} é divísivel por 2!`);
+//   }
+// });
+
+
+// const arrayOFValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+// arrayOFValues.forEach((element, indexOfTheArray, theEntireArray) => {
+//   console.log('Cada elemento do array:', element);
+//   console.log('Index, posição do elemento:', indexOfTheArray);
+//   console.log('Array percorrido:', theEntireArray);
+// });
+
+// // (elemento, index, array)
+
+
