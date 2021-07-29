2c27e834ea04ee6fa3258e16b50d0318c4ad8065
Dogl4 <doougllas@hotmail.com.br>
2021-07-29 15:13:46 -0300

    8.2 conteudo, parteI, HOF, objetos dentro de um array, mudando valores de chaves, de todos usando o for in.

---
+++ 1_Fundamentos/bloco_8/8.2_HOF_forEach,find,some,every,sort/conteudo/HOF_importancia.js
@@ -0,0 +1,47 @@
+// const button = document.getElementById('button');
+// button.addEventListener('click', () => {
+//   console.log('Clicou no botão!');
+// })
+
+
+// const students = [{
+//   name: 'Marte',
+//   grade: 70,
+//   approved: ''
+// }, {
+//   name: 'José',
+//   grade: '56',
+//   approved: ''
+// }, {
+//   name: 'Roberto',
+//   grade: 90,
+//   approved: ''
+// }, {
+//   name: 'Ana',
+//   grade: 90,
+//   approved: ''
+// }];
+
+// function verifyGrades() {
+//   for (let index = 0; index < students.length; index += 1) {
+//     const student = students[index];
+//     if(student.grade >= 60){
+//       student.approved = 'Aprovado';
+//     } else {
+//       student.approved = 'Recuperação';
+//     }
+//   }
+// }
+
+// verifyGrades();
+
+// const verifyGrades = () => {
+//   for (let key in students) {
+//     students[key].grade >= 60 ? students[key].approved = 'Aprovado' : students[key].approved = 'Recuperação';
+//   }
+// };
+
+// verifyGrades();
+
+// console.log(students);
+
