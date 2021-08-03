eeb343c37438253ea12556a3ebe1410fcd6e9147
Dogl4 <doougllas@hotmail.com.br>
2021-08-03 16:04:21 -0300

    exemplo de rest, args.

---
+++ 1_Fundamentos/bloco_8/8.5_7features/conteudo/2rest.js
@@ -0,0 +1,11 @@
+// function quantosParams(...args) {
+//   console.log('parâmetros', args);
+//   return `Você passou ${args.length} parâmetros para a função.`;
+// }
+
+// console.log(quantosParams(0,1,2));
+// console.log(quantosParams('string', null, [1,2,3], { }))
+
+
+const sum = (...args) => args.reduce((acc, curr) => acc + curr, 0);
+console.log(sum(4, 7, 8, 9, 60)); // 88 
