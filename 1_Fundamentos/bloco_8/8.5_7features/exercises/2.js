a9de7fc87cfbc52b5d689c6178a3a6b9c5417f25
Dogl4 <doougllas@hotmail.com.br>
2021-08-03 20:06:54 -0300

    2.js, soma numeros.

---
+++ 1_Fundamentos/bloco_8/8.5_7features/exercises/2.js
@@ -0,0 +1,15 @@
+// 2 Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+const assert = require('assert');
+
+function sum (...numbers) { // tira o [], transforma em somente  números fora de array
+  console.log(numbers); // []
+  return numbers.reduce((acc, curr) => acc + curr, 0);
+};
+console.log(sum(10,12,12));
+
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
