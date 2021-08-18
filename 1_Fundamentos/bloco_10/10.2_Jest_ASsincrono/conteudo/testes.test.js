9dbb68239a661c37f68a5d0601e6130bae9d9953
Dogl4 <doougllas@hotmail.com.br>
2021-08-18 18:40:31 -0300

    async/await com resolve e reject

--- 1_Fundamentos/bloco_10/10.2_Jest_ASsincrono/conteudo/testes.test.js
+++ 1_Fundamentos/bloco_10/10.2_Jest_ASsincrono/conteudo/testes.test.js
@@ -45,3 +45,35 @@ const findAnimalsByType = (type) => (
 //     expect(error).toEqual(new Error('Não possui esse tipo de animal.'))
 //   }
 // })
+
+// test('Testando com async/await, testando o reject', async () => {
+//   expect.assertions(1); // diferença
+//   try {
+//     await findAnimalsByType('Lion');
+//   } catch (error) {
+//     expect(error).toEqual(new Error('Não possui esse tipo de animal.'));
+//   }
+// });
+
+
+
+// async/await com resolves e rejects
+// describe('Testando Async/Await - findAnimalsByType', () => {
+//   describe('Quando o tipo do animal existe', () => {
+//     test('Retorne a lista de animais', async () => {
+//       const listDogs = [
+//         { name: 'Dorminhoco', age: 1, type: 'Dog' },
+//         { name: 'Soneca', age: 2, type: 'Dog' },
+//       ];
+//       await expect(findAnimalsByType('Dog')).resolves.toEqual(listDogs);
+//     });
+//   });
+
+//   describe('Quando o tipo de animal não existe', () => {
+//     test('Retorna um erro', async () => {
+//       expect.assertions(1);
+//       await expect(findAnimalsByType('Lion')).rejects
+//         .toEqual(new Error('Não possui esse tipo de animal.'));
+//     });
+//   });
+// });
