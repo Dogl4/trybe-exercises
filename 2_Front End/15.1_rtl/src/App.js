fb8233fd1f277225bda2582fc523d61e0055005c
Dogl4 <doougllas@hotmail.com.br>
2021-09-24 16:45:36 -0300

    primeiro exemplo de teste usando react-testing-library

---
+++ 2_Front End/15.1_rtl/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <label htmlFor="id-email">
+        Email
+      </label>
+      <input id="id-email" type="email" />
+    </div>
+  );
+}
+
+export default App;
