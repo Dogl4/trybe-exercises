593d0c5309903c4f5db25ed4e8c5d7e94dd89926
Pedro Barreto <85720722+Dogl4@users.noreply.github.com>
2022-01-25 15:23:30 -0300

    Update connection.js

--- 3_Back End/bloco_23_Node/23.3_ArquiteturaWeb_Rest_Restfull/models/connection.js
+++ 3_Back End/bloco_23_Node/23.3_ArquiteturaWeb_Rest_Restfull/models/connection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: 'localhost',
-  user: 'trybe',
-  password: 'trybe123',
+  user: 'seuUsuario',
+  password: 'suaSenha',
   database: 'rest_exercicios'
 });
 
